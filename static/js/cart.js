13561e03aa61a8010f4900cb96087b6038aca920
amilakanishka <amilakanishka@gmail.com>
2020-11-03 13:21:32 +1100

    Update cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,15 +1,15 @@
 d3.select("#submit").on("click", SaveItem);
 
-function SaveItem() {
+function SaveItem() {	
     d3.event.preventDefault();
 	var productSelector = d3.select("#product_name").node().value; 
-	var selectedText = d3.select('#product_name').text();	
+	var selectedText = d3.select('#product_name option:checked').text();	
 	var qty = d3.select("#myInputQuantity").property("value");
 	console.log(qty);
 	console.log(productSelector);
 	console.log(selectedText);
 	localStorage.setItem(productSelector, qty);
-	doShowAll();
+	doShowAll();	
 	
 }
 
