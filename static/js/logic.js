201810971e0733f4aa5b0fa43a4df6389e5b99ef
amilakanishka <amilakanishka@gmail.com>
2020-11-08 15:08:50 +1100

    default recommendations

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,9 @@
 
 var prod_list = [];
+// handling recommendations on first load
+handleDivVisibility("recommend-product-head",false);
+handleDivVisibility("recommend-popular-head",true);
+handleDivVisibility("recommend-customer-head",false);
 
 d3.select("#submit").on("click", handleSubmit);
 
